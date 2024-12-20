b0bb58f28c9d46ab9011f81af2eb2e8f6e64021a
Anh Tu <anhtu@example.comple.com>
2024-12-20 11:06:43 +0700

    edit

--- src/loop_structure/Exercise10.java
+++ src/loop_structure/Exercise10.java
@@ -8,8 +8,8 @@ public class Exercise10 {
         System.out.print("Nhap va so nguyen duong n: ");
         int n = sc.nextInt();
 
-        double sum = 0;
-        for (int i = 0; i < n; i++) {
+        double sum = 0.0;
+        for (int i = 1; i <= n; i++) {
             sum += 1.0 / i;
         }
         System.out.println("Tong = " + sum);
